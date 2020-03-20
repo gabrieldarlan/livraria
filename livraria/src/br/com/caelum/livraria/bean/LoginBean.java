3527e7580a5537eb5a7c8fe9dbecd348edba722a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-20 12:44:10 -0300

    aula 13 - video 1

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -26,10 +27,20 @@ public class LoginBean {
 
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if (existe) {
-			this.usuario=new Usuario();
+
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+
+			this.usuario = new Usuario();
 			return new RedirectView("livro");
 		}
 
+		return new RedirectView("login");
+	}
+
+	public RedirectView logout() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
 		return new RedirectView("login");
 	}
 
